8cc56cc49d91a4f2823f3371c13699bbb8fbb017
ndyky07 <sandipamungkas600@gmail.com>
2025-10-02 14:47:18 +0700

    project dart

---
+++ bin/loop_logic.dart
@@ -0,0 +1,5 @@
+import 'package:loop_logic/loop_logic.dart' as loop_logic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${loop_logic.calculate()}!');
+}
---
+++ test/loop_logic_test.dart
@@ -0,0 +1,8 @@
+import 'package:loop_logic/loop_logic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
