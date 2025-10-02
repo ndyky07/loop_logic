bb29409f70ff9ea82e2547df5164111d60451425
ndyky07 <sandipamungkas600@gmail.com>
2025-10-02 17:54:52 +0700

    feat: tambah menu switch case untuk pilih fitur

--- bin/main.dart
+++ bin/main.dart
@@ -1,25 +1,52 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Masukkan angka: ");
-  int angka = int.parse(stdin.readLineSync()!);
+  while (true) {
+    print("\n=== MENU PROGRAM BILANGAN ===");
+    print("1. Input Angka");
+    print("2. Hitung Pangkat");
+    print("3. Cek Positif/Negatif");
+    print("4. Keluar");
+    stdout.write("Pilih menu: ");
+    int pilihan = int.parse(stdin.readLineSync()!);
 
-  stdout.write("Masukkan pangkat: ");
-  int pangkat = int.parse(stdin.readLineSync()!);
+    switch (pilihan) {
+      case 1:
+        stdout.write("Masukkan angka: ");
+        int angka = int.parse(stdin.readLineSync()!);
+        print("Angka yang dimasukkan adalah $angka");
+        break;
 
-  // Hitung pangkat
-  int hasil = 1;
-  for (int i = 0; i < pangkat; i++) {
-    hasil *= angka;
-  }
-  print("$angka pangkat $pangkat = $hasil");
+      case 2:
+        stdout.write("Masukkan angka: ");
+        int angka = int.parse(stdin.readLineSync()!);
+        stdout.write("Masukkan pangkat: ");
+        int pangkat = int.parse(stdin.readLineSync()!);
+        int hasil = 1;
+        for (int i = 0; i < pangkat; i++) {
+          hasil *= angka;
+        }
+        print("$angka pangkat $pangkat = $hasil");
+        break;
 
-  // Cek positif, negatif, atau nol
-  if (angka > 0) {
-    print("$angka adalah bilangan POSITIF.");
-  } else if (angka < 0) {
-    print("$angka adalah bilangan NEGATIF.");
-  } else {
-    print("Angka yang dimasukkan adalah NOL.");
+      case 3:
+        stdout.write("Masukkan angka: ");
+        int angka = int.parse(stdin.readLineSync()!);
+        if (angka > 0) {
+          print("$angka adalah bilangan POSITIF.");
+        } else if (angka < 0) {
+          print("$angka adalah bilangan NEGATIF.");
+        } else {
+          print("Angka yang dimasukkan adalah NOL.");
+        }
+        break;
+
+      case 4:
+        print("Terima kasih, program selesai.");
+        return;
+
+      default:
+        print("Pilihan tidak valid.");
+    }
   }
 }
