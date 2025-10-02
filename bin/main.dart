bed7213e0f0256968a376d28154d1a0f3b112fe1
ndyky07 <sandipamungkas600@gmail.com>
2025-10-02 16:21:58 +0700

    feat: tambah input angka dari user

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
 void main() {
   print("=== Program Operasi Bilangan ===");
+  stdout.write("Masukkan sebuah angka: ");
+  int angka = int.parse(stdin.readLineSync()!);
+
+  print("Angka yang dimasukkan adalah $angka");
 }
