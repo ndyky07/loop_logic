d55c192f8b95d02260aab6a205ff41a372ff31c3
ndyky07 <sandipamungkas600@gmail.com>
2025-10-02 17:23:01 +0700

    feat: tambah fitur cek positif negatif

--- bin/main.dart
+++ bin/main.dart
@@ -7,10 +7,19 @@ void main() {
   stdout.write("Masukkan pangkat: ");
   int pangkat = int.parse(stdin.readLineSync()!);
 
+  // Hitung pangkat
   int hasil = 1;
   for (int i = 0; i < pangkat; i++) {
     hasil *= angka;
   }
-
   print("$angka pangkat $pangkat = $hasil");
+
+  // Cek positif, negatif, atau nol
+  if (angka > 0) {
+    print("$angka adalah bilangan POSITIF.");
+  } else if (angka < 0) {
+    print("$angka adalah bilangan NEGATIF.");
+  } else {
+    print("Angka yang dimasukkan adalah NOL.");
+  }
 }
